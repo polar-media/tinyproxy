f059a7845038e48ecd5814a52886325a3f53e1fe
Peter Szoke <szokeptr@gmail.com>
2019-12-09 13:50:14 +0100

    Update gitignore

---
+++ src/ip-tools.c
@@ -0,0 +1,5 @@
+//
+// Created by Peter Szoke on 12/9/19.
+//
+
+#include "ip-tools.h"
---
+++ src/ip-tools.h
@@ -0,0 +1,8 @@
+//
+// Created by Peter Szoke on 12/9/19.
+//
+
+#ifndef TINYPROXY_IP_TOOLS_H
+#define TINYPROXY_IP_TOOLS_H
+
+#endif //TINYPROXY_IP_TOOLS_H
